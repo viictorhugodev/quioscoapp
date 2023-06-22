099852bb7ee8a8fe849628a3b92594c7fd16fe3d
Victor Hugo Segundo Aguilar <viictorhugo.dev@gmail.com>
2023-06-22 11:39:51 -0600

    Deploy proyecto

---
+++ components/Sidebar.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image"
+import useQuiosco from "@/hooks/useQuiosco"
+import Categoria from "./Categoria"
+
+const Sidebar = () => {
+
+  const {categorias} = useQuiosco()
+
+  // console.log('Las categorias', categorias)
+
+  return (
+    <>
+      <Image 
+        width={300} 
+        height={100} 
+        src="/assets/img/logo.svg" 
+        alt="Imagen Logo"
+      />
+
+      <nav className="mt-10">
+        {categorias.map(categoria => (
+          <Categoria
+            key={categoria.id}
+            categoria={categoria}
+          />
+        ))}
+      </nav>
+    </>
+  )
+}
+
+export default Sidebar
---
+++ helpers/index.js
@@ -0,0 +1,7 @@
+export const formatearDinero = cantidad => {
+  return cantidad.toLocaleString('es-MX', { 
+    style: 'currency', 
+    currency: 'MXN' 
+  });
+}
+
